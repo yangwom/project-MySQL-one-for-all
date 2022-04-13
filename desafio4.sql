ae3431a833863c98ed9f32a1cb43a18043d5b4c3
yang vieira <yangflix3@gmail.com>
2022-04-13 16:14:52 -0400

    desafio04

---
+++ desafio4.sql
@@ -0,0 +1,7 @@
+SELECT usr.nome AS 'usuario', 
+IF(max(re.data_de_reproducao) LIKE '2021%', 'Usuário ativo', 'Usuário inativo') AS condicao_usuario
+FROM users AS usr
+JOIN reproducoes AS re
+ON usr.usuario_id = re.usuario_id
+GROUP BY usr.usuario_id
+ORDER BY usr.nome;
