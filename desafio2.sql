bdc55aaeb4505641640bb5aee5163c517b74e4ec
yang vieira <yangflix3@gmail.com>
2022-04-13 12:27:14 -0400

    desafio 2

--- desafio2.sql
+++ desafio2.sql
@@ -1,5 +1,5 @@
 SELECT 
 COUNT(DISTINCT m.cancoes) AS 'cancoes',
-COUNT(DISTINCT a.nome) AS 'artista',
+COUNT(DISTINCT a.nome) AS 'artistas',
 COUNT(DISTINCT alb.album_name) AS 'albuns'
-FROM musicas AS m, artista AS a, album AS alb; 
+FROM musicas AS m , artista AS a, album AS alb; 
